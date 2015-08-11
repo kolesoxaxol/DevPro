0ad0e39331e87f93940c0c52767bc230b946b106
Yevhenii Koliesnik <dp260793kev@yandex.ua>
2015-08-11 11:42:41 +0300

    Add GenericService UnitOFWork repository  install AgilityPAck  add TDD project

---
+++ DevPro/BusinessLogic/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿
+using DAL;
+using Model.Entities;
+
+namespace BusinessLogic.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        void Dispose();
+        void Save();
+        GenericRepository<Feed> FeedRepository { get; set; }
+        GenericRepository<Resource> ResourceRepository { get; set; }
+    }
+}
---
+++ DevPro/BusinessLogic/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using DAL;
+using Model.Entities;
+
+namespace BusinessLogic.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly NewsAggregatorContext _context;
+        private GenericRepository<Feed> _feedRepository;
+        private GenericRepository<Resource> _resourceRepository;
+
+        public UnitOfWork(NewsAggregatorContext context)
+        {
+            _context = context;          
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        private bool _disposed;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+
+                }
+            }
+            _disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+
+        public GenericRepository<Feed> FeedRepository
+        {
+            get { return _feedRepository ?? (_feedRepository = new GenericRepository<Feed>(_context)); }
+            set
+            {
+                _feedRepository = value;
+            }
+        }
+
+        public GenericRepository<Resource> ResourceRepository
+        {
+            get { return _resourceRepository ?? (_resourceRepository = new GenericRepository<Resource>(_context)); }
+            set
+            {
+                _resourceRepository = value;
+            }
+        }
+    }
+}
